05f5c17b565347e9331a6a992c3885a0468cada3
apekshajain05 <62800258+apekshajain05@users.noreply.github.com>
2021-09-01 19:03:19 +0530

    Add files via upload

---
+++ js/CursorEvent.js
@@ -0,0 +1,42 @@
+AFRAME.registerComponent('cursor-event',{
+    schema:{
+        selectedItemId:{default:'',type:'string'}
+    },
+    init:function(){
+        this.handleMouseEnterEvent()
+        this.handleMouseLeaveEvents()
+    },
+    handlePlacesListState:function(){
+        
+        const id=this.el.getAttribute('id')
+        const placesId=['taj-mahal','budapest','new-york-city','eiffel-tower']
+        if(placesId.includes(id)){
+            const placesContainer=document.querySelector('#places-container')
+            placesContainer.setAttribute('cursor-listener',{
+                selectedItemId:id
+            })
+            this.el.setAttribute('material',{color:'black',opacity:1})
+        }
+    },
+    handleMouseEnterEvent:function(){
+        this.el.addEventListener('mouseenter',()=>{
+            this.handlePlacesListState()
+        })
+    },
+    handleMouseLeaveEvents:function(){
+        this.el.addEventListener('mouseleave',()=>{
+            const {selectedItemId}=this.data;
+            if(selectedItemId){
+                const el=document.querySelector(`#${selectedItemId}`)
+                const id=el.getAttribute('id')
+                if(id==selectedItemId){
+                    el.setAttribute('material',{
+                        color:'white',
+                        opacity:1
+                    })
+                }
+            }
+        })
+    }
+
+})
